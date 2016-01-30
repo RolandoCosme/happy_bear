bb0edfd878c1104dab76189a5519616cb2efb028
Rolando Cosme <rolandocosme@nbp-54-121.nbp.ruw.rutgers.edu>
2016-01-30 13:32:01 -0500

    added index.js commands to insert data to table

--- index.js
+++ index.js
@@ -9,12 +9,27 @@ var connection = mysql.createConnection({
 });
  
 connection.connect();
- 
-connection.query('SELECT happy_bears.name, favorite_food, personality FROM happy_bears;', function(err, rows, fields) {
-  if (err) throw err;
-  for ( var i = 0; i < rows.length; i++) {
-    console.log(rows[i].name + " is " + rows[i].personality);
-  }
-});
+
+var showBears = function () {
+  connection.query('SELECT * FROM happy_bears;', function(err, rows, fields) {
+    if (err) throw err;
+   // console.log(rows);
+    for ( var i = 0; i < rows.length; i++) {
+      console.log(rows[i].name + " is " + rows[i].personality);
+    }
+  });
+}
+
+var addBears = function () {
+  connection.query('INSERT INTO happy_bears (name, favorite_food, personality) VALUES (?,?,?)', ["Smoky", "beef jerky", "mean"], function(err, results) {
+    if (err) throw err;
+    console.log("inserted");
+  });
+}
+
+
+
+showBears();
+addBears();
  
 connection.end();
